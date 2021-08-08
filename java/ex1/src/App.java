529d52fa5afe938a5837a176f15aa26a15073781
ojhdt <ojhdtmail@gmail.com>
2021-08-09 00:54:24 +0800

    update

--- java/ex1/src/App.java
+++ java/ex1/src/App.java
@@ -1,8 +1,10 @@
+
 public class App {
+    public static <T> T count(T[] arr){
+        return arr[arr.length-1];
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        Student stu1 = new Student("Jack");
-        stu1.setNum(1);
-        stu1.outPut();
+        Integer []intt = {1,2,3,4,5};
+        System.out.println(count(intt));
     }
 }
